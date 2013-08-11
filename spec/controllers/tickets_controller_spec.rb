a62da0069ac2b9db4a3b0baeaa7e3839400dff81
Ted Ewanchyna <ted.ewanchyna@gmail.com>
2013-08-11 14:54:34 +0000

    Restricting ticket updating to only those who have permission

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -16,6 +16,11 @@ describe TicketsController do
         response.should redirect_to(project)
         flash[:alert].should eql("You cannot create tickets on this project.")
       end
+      def cannot_update_tickets!
+        response.should redirect_to(project)
+        flash[:alert].should eql("You cannot edit tickets on this project.")
+      end
+
       it "cannot begin to create a ticket" do
         get :new, :project_id => project.id
         cannot_create_tickets!
@@ -24,6 +29,19 @@ describe TicketsController do
         post :create, :project_id => project.id
         cannot_create_tickets!
       end
+
+      it "cannot edit a ticket without permission" do
+        get :edit, { :project_id => project.id, :id => ticket.id }
+        cannot_update_tickets!
+      end
+      it "cannot update a ticket without permission" do
+        put :update, { :project_id => project.id,
+                       :id => ticket.id,
+                       :ticket => {}
+                     }
+        cannot_update_tickets!
+      end
+
     end
 
     it "cannot access a ticket for a project" do
