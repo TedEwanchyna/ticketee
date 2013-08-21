b1f524c902254422c1f385362b1294a251d30531
Ted Ewanchyna <ted.ewanchyna@gmail.com>
2013-08-21 11:54:22 +0000

    Add Receiver class to receive emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -5,7 +5,13 @@ class Notifier < ActionMailer::Base
   def comment_updated(comment, user)
     @comment = comment
     @user = user
-    mail(:to => user.email, :from => "solicadmi@gmail.com",
+#    mail(:to => user.email, :from => "solicadmi+#{comment.project.id}+#{comment.ticket_id}@gmail.com",
+#         :subject => "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
+#    mail(:to => user.email, :from => "Ticketee <solicadmi+#{comment.project.id}+#{comment.ticket_id}@gmail.com>",
+    from = "solicadmi+#{comment.project.id}+#{comment.ticket_id}@gmail.com"
+    reply_to = "Ticketee <solicadmi+#{comment.project.id}+#{comment.ticket_id}@gmail.com>"
+
+    mail(:to => user.email, :reply_to => reply_to, :from => from,
          :subject => "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
     end
 
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Notifier do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "correctly sets the reply-to" do
+    comment = Factory(:comment)
+    mail = ActionMailer::Base.deliveries.last
+#    mail.from.should eql(["Ticketee <solicadmi+#{comment.project.id}+" +
+#      "#{comment.ticket.id}@gmail.com>"])
+    mail.from.should eql(["solicadmi+#{comment.project.id}+" +
+      "#{comment.ticket.id}@gmail.com"])
+  end
 end
