887cbd0b5fde84b279556ecae851a80e1b12e348
Ted Ewanchyna <ted.ewanchyna@gmail.com>
2013-08-30 20:41:31 +0000

    trying to get production environment on server to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,16 @@ Ticketee::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+#  config.action_mailer.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :user_name            => 'solicadmi@gmail.com',
+#  :password             => 'sustain4ever',
+#  :tts => true  }
 
   # Enable threaded mode
   # config.threadsafe!
